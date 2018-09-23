73cc3d0e184cc327af39ef1fedbac27132953d2e
wyattscarpenter <wyattscarpenter@gmail.com>
2018-09-23 14:45:30 -0500

    misc
    adjust podsplit time to 10 minutes, among other things

--- vid-cat.bat
+++ vid-cat.bat
@@ -1,4 +1,5 @@
+SET outfile=%1
+IF "%outfile%"=="" (SET outfile=out.mkv)
 (for %%i in (*.*) do @echo file '%%i') > vidlist.tmp
-ffmpeg -safe 0 -f concat -i vidlist.tmp -c copy out.mkv
-
-REM del vidlist.tmp
+ffmpeg -safe 0 -f concat -i vidlist.tmp -c copy %outfile%
+del vidlist.tmp

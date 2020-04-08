b0b4a961b2a4517d63a235fee395785bb6950a5f
wyattscarpenter <wyattscarpenter@gmail.com>
2020-04-08 05:42:42 -0500

    Include note so I can remember how to replicate this.

--- cl.bat
+++ cl.bat
@@ -3,4 +3,6 @@ REM so we invoke this batch script that puts all the correct tools into scope
 REM and then invoke the actual cl.
 REM This script named cl is hidden by the actual cl after a single invocation,
 REM so subsequent invocations of cl simply refer to the real cl.
+REM This script is super path-dependent.
+REM You can get the VS build tools from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" & cl %*

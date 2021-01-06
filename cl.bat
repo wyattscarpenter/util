0c91fa80569185279956e8121e70ab0c30068637
wyattscarpenter <wyattscarpenter@gmail.com>
2021-01-06 17:45:34 +0000

    switch to x64 cl (used to get 32 bit x86)

--- cl.bat
+++ cl.bat
@@ -5,4 +5,4 @@ REM This script named cl is hidden by the actual cl after a single invocation,
 REM so subsequent invocations of cl simply refer to the real cl.
 REM This script is super path-dependent.
 REM You can get the VS build tools from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" & cl %*
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" & cl %*

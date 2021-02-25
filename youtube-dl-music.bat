1c0046c77c07d1f3ec97a97513277e23f0651ffc
wyattscarpenter <wyattscarpenter@gmail.com>
2021-02-25 18:41:41 +0000

    misc

--- youtube-dl-music.bat
+++ youtube-dl-music.bat
@@ -1,6 +1,6 @@
-REM rip music from youtube like a sane person might
+REM rip playlist from youtube like a sane person might
 REM with sensible defaults instead of garbage ones
 REM takes one url as an argument, you have to quote it (bash eats ampersands)
 REM but if you like you can also insert your own modifcations on the end since the arguments are unquoted
 
-youtube-dl -f m4a -o "%%(autonumber)s %%(title)s (youtube rip).%%(ext)s" %*
+youtube-dl -f m4a -o "%%(autonumber)s %%(title)s %(display_id)s (youtube rip).%%(ext)s" %*

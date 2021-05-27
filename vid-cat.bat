3bc6167048c120ec100c0a22c0315fc179d994e1
wyattscarpenter <wyattscarpenter@gmail.com>
2021-05-28 00:49:09 +0100

    I'm sure these are great

--- vid-cat.bat
+++ vid-cat.bat
@@ -1,5 +1,5 @@
 SET outfile=%1
 IF "%outfile%"=="" (SET outfile=out.mkv)
-(for %%i in (*.*) do @echo file '%%i') > vidlist.tmp
-ffmpeg -safe 0 -f concat -i vidlist.tmp -c copy %outfile%
-del vidlist.tmp
+(for %%i in (*.*) do @echo file '%%i') > vidlist.tmp.txt
+ffmpeg -safe 0 -f concat -i vidlist.tmp.txt -c copy %outfile%
+del vidlist.tmp.txt

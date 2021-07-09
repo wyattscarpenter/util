251b93e743baa344a8e8fbcc374ec931016799fb
wyattscarpenter <wyattscarpenter@gmail.com>
2021-07-09 16:04:55 +0100

    add colons to kill-time

--- discord-kill-time.user.js
+++ discord-kill-time.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Kill Time
 // @namespace    https://github.com/wyattscarpenter/util
-// @version      2
+// @version      3
 // @author       wyattscarpenter
 // @description  Discord text formatting don't be annoying challenge
 // @match        *://discordapp.com/*
@@ -13,10 +13,14 @@
 //It seems to be quite finicky and I wouldn't have gotten it on my own.
 //His userscript says it's GNU AGPLv3 so maybe that implies this one is also? I don't really know.
 
-function killTime(){ //should work for both compact and cozy. I forget what is for which.
-  document.querySelectorAll('[class^="latin24CompactTimeStamp"]').forEach(element => element.style.userSelect = "none")
-  document.querySelectorAll('[class^="timestamp"]').forEach(element => element.style.userSelect = "none")
-  document.querySelectorAll('[class^="separator"]').forEach(element => element.style.userSelect = "none")
+function killTime(){ //should work for both compact and cozy.
+  document.querySelectorAll('[class^="latin24CompactTimeStamp"]').forEach(element => element.style.userSelect = "none") //compact mode timestamp
+  document.querySelectorAll('[class^="timestamp"]').forEach(element => element.style.userSelect = "none") //cozy mode timestamp
+  document.querySelectorAll('[class^="separator"]').forEach(element => element.remove()) //since separator location differs between cozy and compact, it's best just to not use it.
+  //Since I still want a copyable colon in cozy mode, we add one:
+  //(We also change the class as a lazy way of preventing an infinite colon-adding loop.)
+  document.querySelectorAll('[class^="headerText"]').forEach(element => {element.append(": "); element.className = "colonatedHeaderText";})
+  //TODO: In compact on firefox, there is an unfortunate space between the username and the :, caused by a "whitespace only text node" in there...
 }
 
 var observer, observing, selector = '[class^="chat-"]'; //'[class^="scrollerInner-"]'; //this also works, as per https://greasyfork.org/en/scripts/373445-discord-keyword-notification/discussions/60844

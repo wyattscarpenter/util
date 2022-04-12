61df6e1118902d18b89594897eeb09a99a2a0a1a
wyattscarpenter <wyattscarpenter@gmail.com>
2022-04-12 12:18:42 -0700

    native batch version of text command

---
+++ text.bat
@@ -0,0 +1,3 @@
+REM This command shows you the text of a command that is on your path.
+REM Please excuse the crazy hacks batch makes one do to use output as a parameter later...
+for /F "usebackq delims=" %%N in (`where %1`) do type %%N

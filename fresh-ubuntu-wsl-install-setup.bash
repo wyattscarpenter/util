e203ad084c16f240a9801b0d55e80fc3692b0855
wyattscarpenter <wyattscarpenter@gmail.com>
2022-10-29 16:18:43 -0700

    more documentation and steps for ubuntu set-up script

--- fresh-ubuntu-wsl-install-setup.bash
+++ fresh-ubuntu-wsl-install-setup.bash
@@ -1,6 +1,10 @@
+# .bashrc stuff for WSL X server:
+echo #This next line lets WSL project to x server running as a windows application (exe). >> ~/.bashrc
+echo export DISPLAY=localhost:0.0 >> ~/.bashrc
+
+# Ubuntu repository and software stuff:
 sudo add-apt-repository main
 sudo add-apt-repository universe
 sudo add-apt-repository restricted
 sudo add-apt-repository multiverse
-echo #This next line lets WSL project to x server running as a windows application (exe).
-echo export DISPLAY=localhost:0.0 >> ~/.bashrc
+sudo apt update && sudo apt upgrade && sudo apt autoremove

ba944f04b6eb00434cf9e1d30643605f675f53ee
wyattscarpenter <wyattscarpenter@gmail.com>
2022-12-01 15:27:03 -0800

    add lines to make file perms work

--- fresh-ubuntu-wsl-install-setup.bash
+++ fresh-ubuntu-wsl-install-setup.bash
@@ -2,6 +2,12 @@
 echo #This next line lets WSL project to x server running as a windows application (exe). >> ~/.bashrc
 echo export DISPLAY=localhost:0.0 >> ~/.bashrc
 
+#sudo zone I guess!
+#makes chmod/chown actually do something:
+sudo -s #the below don't work with regular sudo, although you could use tee instead.
+sudo echo [automount] >>/etc/wsl.conf
+sudo echo options = "metadata" >>/etc/wsl.conf
+
 # Ubuntu repository and software stuff:
 sudo add-apt-repository main
 sudo add-apt-repository universe

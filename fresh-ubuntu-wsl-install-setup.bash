9f95abb948171a2c6023ed5280f83aeaf083688d
wyattscarpenter <wyattscarpenter@gmail.com>
2022-12-07 18:43:32 -0800

    advance sophistication of bashrc stuff and fix unmark-exe

--- fresh-ubuntu-wsl-install-setup.bash
+++ fresh-ubuntu-wsl-install-setup.bash
@@ -1,9 +1,16 @@
-# .bashrc stuff for WSL X server:
-echo #This next line lets WSL project to x server running as a windows application (exe). >> ~/.bashrc
-echo export DISPLAY=localhost:0.0 >> ~/.bashrc
+#bashrc stuff
+echo >>~/.bashrc
+echo \#Stuff from the script collection Util: >>~/.bashrc
+echo \#This next line lets WSL project to an X server running as a windows application (exe): >>~/.bashrc
+echo export DISPLAY=localhost:0.0 >>~/.bashrc
+#This lets you exit the shell with the pseudo-command "q"
+alias-q-exit
+#Make bash command prompt always show the status returned by the previously-run command
+make-bash-prompt-show-status
+echo >>~/.bashrc
 
+#Makes chmod/chown actually do something:
 #sudo zone I guess!
-#makes chmod/chown actually do something:
 sudo -s #the below don't work with regular sudo, although you could use tee instead.
 sudo echo [automount] >>/etc/wsl.conf
 sudo echo options = "metadata" >>/etc/wsl.conf

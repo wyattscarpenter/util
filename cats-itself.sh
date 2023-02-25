e3e9c6a66b9096a5e22539e56f3fd1f8d5956a66
wyattscarpenter <wyattscarpenter@gmail.com>
2023-02-25 02:29:50 -0800

    a file that cats itself

---
+++ cats-itself.sh
@@ -0,0 +1,2 @@
+#!/bin/cat
+Here are some more lines to prove that this file cats itself instead of doing anything else. I learned about this hack from mrmtonio on Stack Overflow, who apparently discovered it on his own (although he is likely not the first to discover such a thing) https://stackoverflow.com/a/71934101/20658329

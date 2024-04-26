6b7ca87b010fbfd083a5a7945081b8f62d1778f8
wyattscarpenter <wyattscarpenter@gmail.com>
2024-04-25 22:52:53 -0400

    M text.bat {+@echo off+}

--- text.bat
+++ text.bat
@@ -1,3 +1,4 @@
+@echo off
 REM This command shows you the text of a command that is on your path.
 REM Please excuse the crazy hacks batch makes one do to use output as a parameter later...
 for /F "usebackq delims=" %%N in (`where %1`) do type %%N

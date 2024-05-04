502215ab6eeb5705edc9fbd13ebf7fae607e94cf
wyattscarpenter <wyattscarpenter@gmail.com>
2024-05-04 04:07:59 -0400

    use  https://github.com/Multarix/Discord-Hide-Blocked-Messages to figure out how to hide blocked messages more consistently, and also spin off discord-triple-block to its own script

--- discord-triple-block.user.js
+++ discord-triple-block.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Discord Double Block
+// @name         Discord Triple Block
 // @namespace    https://github.com/wyattscarpenter/util
 // @version      5
 // @author       wyattscarpenter
-// @description  Removes "message blocked" message from blocked messages in discord.
+// @description  Removes "message blocked" message from blocked messages in discord, COMPLETELY.
 // @match        *://discordapp.com/*
 // @match        *://discord.com/*
 // @grant        none
@@ -11,6 +11,6 @@
 
 //I am indebted to https://github.com/Multarix/Discord-Hide-Blocked-Messages for figuring out how to do this.
 
-const css = '[class^="blockedMessageText"] { display: none; }'
+const css = '[class^="blockedSystemMessage"] { display: none; }'
 const style = document.createElement('style'); style.innerHTML = css;
 document.body.appendChild(style);

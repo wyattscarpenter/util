c0f31dcb9bc119a99fb81ddc97daa5b92d6e7952
wyattscarpenter <wyattscarpenter@gmail.com>
2024-07-09 10:48:48 -0500

    upgrade cl version

--- cl.bat
+++ cl.bat
@@ -1,8 +1,2 @@
-REM For some reason, the windows c compile, cl, is troublesome to run by itself
-REM so we invoke this batch script that puts all the correct tools into scope
-REM and then invoke the actual cl.
-REM This script named cl is hidden by the actual cl after a single invocation,
-REM so subsequent invocations of cl simply refer to the real cl.
-REM This script is super path-dependent.
-REM You can get the VS build tools from https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" & cl %*
+REM For some reason, the Windows c compiler, cl, is troublesome to run by itself, so we invoke this batch script that puts all the correct tools into scope, and then invoke the actual cl. This script named cl is hidden by the actual cl after a single invocation, so subsequent invocations of cl simply refer to the real cl. This script is super path-dependent. You can get the VS build tools from https://aka.ms/vs/17/release/vs_BuildTools.exe , or perhaps https://visualstudio.microsoft.com/downloads/?q=build+tools or https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools .
+"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" && cl.exe %*

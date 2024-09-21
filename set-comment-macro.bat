caaf1b649c8f8be745d8bfc8989ca6c61e511a32
wyattscarpenter <wyattscarpenter@gmail.com>
2024-09-20 22:00:08 -0700

    simplify the exit macros for ps
    realized I was only using this for the $([Char]4) thing, which I could easily remove

--- set-comment-macro.bat
+++ set-comment-macro.bat
@@ -3,6 +3,5 @@ REM This batch script sets cmd autorun to use doskey to make // a macro for the 
 reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "doskey //=REM"
 
 REM Alias REM and // to be comments in powershell (perhaps we could call this a pseudo-comment). I don't think side-effects in the commented expressions evaluate, which is nice. Also note that // must be followed by a space. The comment-ness will be terminated by ;, &&, etc, unfortunately.
-powershell "echo 'Invoke-Expression \"Function // { }\"' >> $profile"
-powershell "echo 'Invoke-Expression \"Function REM { }\"' >> $profile"
-
+powershell "echo 'Function // { }' >> $profile"
+powershell "echo 'Function REM { }' >> $profile"
--- set-exit-hotkey-command-macro.bat
+++ set-exit-hotkey-command-macro.bat
@@ -5,12 +5,12 @@ REM (Note that I tried, instead of the autorun method, putting this batch script
 
 reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "doskey  =exit"
 
-REM also do powershell quit and ^W and ^D
+REM also do powershell q and ^W and ^D
 REM it's for only the current user, because that's a very convenient command
 REM the syntax here is complicated, but simpler ideas didn't work for me. See https://stackoverflow.com/a/74648083/ for my source on this.
 REM incidentally, you can clear (delete completely) the current powershell profile file with `del $profile` in powershell. (it will probably run q.bat if you q after this, which is useless).
-powershell "echo 'Invoke-Expression \"Function $([Char]4) { Invoke-Command -ScriptBlock { Exit } }\"' >> $profile"
-powershell "echo 'Invoke-Expression \"Function  { Invoke-Command -ScriptBlock { Exit } }\"' >> $profile"
-powershell "echo 'Invoke-Expression \"Function q { Invoke-Command -ScriptBlock { Exit } }\"' >> $profile"
+powershell "echo 'Function  { Invoke-Command -ScriptBlock { Exit } }' >> $profile"
+powershell "echo 'Function  { Invoke-Command -ScriptBlock { Exit } }' >> $profile"
+powershell "echo 'Function q { Invoke-Command -ScriptBlock { Exit } }' >> $profile"
 
 REM by the way, if you're running this from powershell instead of cmd (first of all, the REMs have errored so far, I guess; and also the quoting was wrong on the powershell lines), then to use the updated profile run: . $profile

9f7d3c3f657b3886daf354f4614d23f26f79c874
wyattscarpenter <wyattscarpenter@gmail.com>
2024-09-20 18:14:35 -0700

    add pseudo-comments

---
+++ set-comment-macro.bat
@@ -0,0 +1,8 @@
+REM This batch script sets cmd autorun to use doskey to make // a macro for the REM (remark, ie comment) command. Note that it does this very poorly, because //foo doesn't work as a comment.
+
+reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "doskey //=REM"
+
+REM Alias REM and // to be comments in powershell (perhaps we could call this a pseudo-comment). I don't think side-effects in the commented expressions evaluate, which is nice. Also note that // must be followed by a space. The comment-ness will be terminated by ;, &&, etc, unfortunately.
+powershell "echo 'Invoke-Expression \"Function // { }\"' >> $profile"
+powershell "echo 'Invoke-Expression \"Function REM { }\"' >> $profile"
+
--- set-slashes-comment-macro.bat
+++
@@ -1,3 +0,0 @@
-REM This batch script sets cmd autorun to use doskey to make // a macro for the REM (remark, ie comment) command. Note that it does this very poorly, because //foo doesn't work as a comment.
-
-reg add "HKCU\Software\Microsoft\Command Processor" /v Autorun /d "doskey //=REM"

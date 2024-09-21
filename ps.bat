eefed30d7ad98e52eac050ce0061bb03e27290a1
wyattscarpenter <wyattscarpenter@gmail.com>
2024-09-20 18:13:45 -0700

    don't need the natter from ps each time...

--- ps.bat
+++ ps.bat
@@ -1,2 +1,3 @@
+@echo off
 REM this command lets you issue powershell command from the cmd command line; essentially it's an alias for `powershell`. If you input commands powershell will execute them and return control to you, like powershell -command; otherwise the powershell interactive session will begin. This is just literally how `powershell` works; I don't know why I'm documenting this as well.
 powershell %*

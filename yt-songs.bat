ef71122dc42c37fffd2db6fb6b5a8a979a241ee4
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-20 21:27:06 -0700

    (untested) make yt.bat and yt-music.bat follow a more-consistent scheme

--- yt-songs.bat
+++ yt-songs.bat
@@ -1,6 +1,6 @@
 REM rip playlist from youtube like a sane person might
 REM with sensible defaults instead of garbage ones
-REM takes one url as an argument, you have to quote it (bash eats ampersands)
+REM takes one url as an argument, you have to quote it (batch eats ampersands)
 REM but if you like you can also insert your own modifcations on the end since the arguments are unquoted
 
-yt-dlp -f m4a -o "%%(autonumber)s %%(title)s %%(display_id)s (youtube rip).%%(ext)s" %*
+yt-song -o "%%(autonumber)s %%(title)s %%(display_id)s (youtube rip).%%(ext)s" %*

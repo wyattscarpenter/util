4e4ced02ec11bc430ab9adcb541c5825ccfd373e
wyattscarpenter <wyattscarpenter@gmail.com>
2025-03-21 01:05:22 -0700

    clarify top documentation in yt-song\(s\) batch files

--- yt-song.bat
+++ yt-song.bat
@@ -1,2 +1,4 @@
-REM I use this script to download songs from youtube (many obscure ones aren't offered anywhere else!)
+REM I use this script to download songs from youtube (many obscure ones aren't offered anywhere else!).
+REM If you like you can also insert your own flags to yt-dlp on the end.
+
 yt-dlp --embed-thumbnail --add-metadata -f m4a %*
--- yt-songs.bat
+++ yt-songs.bat
@@ -1,6 +1,5 @@
-REM rip playlist from youtube like a sane person might
-REM with sensible defaults instead of garbage ones
-REM takes one url as an argument, you have to quote it (batch eats ampersands)
-REM but if you like you can also insert your own modifcations on the end since the arguments are unquoted
+REM Rip playlist from youtube like a sane person might, with sensible defaults instead of garbage ones.
+REM Takes one url as an argument; you have to quote it (batch eats ampersands).
+REM If you like you can also insert your own flags to yt-dlp on the end.
 
 yt-song -o "%%(autonumber)s %%(title)s %%(display_id)s (youtube rip).%%(ext)s" %*

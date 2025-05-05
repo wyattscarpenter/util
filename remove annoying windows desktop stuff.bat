e8b3e42b36e46e3490b40da756f85eec6317ce07
wyattscarpenter <wyattscarpenter@gmail.com>
2025-05-05 01:41:54 -0700

    do the windows annoying stuff more correctly

--- remove annoying windows desktop stuff.bat
+++ remove annoying windows desktop stuff.bat
@@ -1,7 +1,11 @@
-REM This bat file removes the annoying auto-created files from the Windows 10 desktop. It does not delete anything that I think you need, even if that thing you need is still clutter. However, this is still based on my personal judgement that you don't need stuff like .lesshst
-REM I keep this file here, but put a shortcut to it on my desktop, so I can just click that.
-REM TODO: I think the "right" way to do this would be to delete the files I know about, and then rmdir the folders, so that if there are other files in here then they don't get accidentally deleted. Just in case. This would also let me add Videos/ to this list, which occasionally has video captures in it but is normally just annoyingly emptily created.
-wsl rf Music .lesshst .matplotlib .streamlit Searches Favorites
-REM so, things should be done like this:
-del "%USERPROFILE%/.python_history"
+REM This bat file removes the annoying auto-created files from the Windows 10 desktop. It does not delete anything that I think you need, even if that thing you need is still clutter. However, this is still based on my personal judgement that you don't need stuff like .lesshst. I keep this file in util, but put a shortcut to it on my desktop, so I can just click that. It should be run in administrator mode (TODO: a number of these still give "Access is denied." errors for apparently no reason). The "right" way to do this is to delete the files I know about, and then rmdir the folders, so that only known-bad files are and empty dirs deleted. Just in case. (TODO: I haven't been able to hone in on exactly the right things because I keep getting the Access denied errors.)
+rmdir "%USERPROFILE%\Music"
+rmdir "%USERPROFILE%\Searches"
+rmdir "%USERPROFILE%\Favorites"
+rmdir "%USERPROFILE%\.streamlit"
+rmdir "%USERPROFILE%\.matplotlib"
+del "%USERPROFILE%\.lesshst"
+del "%USERPROFILE%\.python_history"
+REM This occasionally has video captures in it but is normally just annoyingly emptily created:
+rmdir "%USERPROFILE%\Videos"
 pause

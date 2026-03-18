33177174d213f92e43ccf4f9170d78b78bc57ee0
wyattscarpenter <wyattscarpenter@gmail.com>
2026-03-18 08:37:17 -0500

    muck about with yt-dlp new flags more

--- yt.bat
+++ yt.bat
@@ -1,3 +1,4 @@
-REM a simple alias for yt-dlp (or youtube-dl or any other project carrying the mantle, agnostically)
+REM A simple alias for yt-dlp (or youtube-dl or any other project carrying the mantle — opqauely to the caller).
+REM Things got complicated enough with cookies and js runtimes that I've just specified those here.
 
-yt-dlp %*
+yt-dlp --cookies-from-browser firefox --js-runtime node --remote-components ejs:github %*

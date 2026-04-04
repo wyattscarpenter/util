a41ffe6f3757cf49cd0aa85e4a83c5d2e8d4ca64
wyattscarpenter <wyattscarpenter@gmail.com>
2026-04-03 23:19:13 -0500

    make more progress on the annoying windows desktop stuff

--- remove annoying windows desktop stuff.bat
+++ remove annoying windows desktop stuff.bat
@@ -1,5 +1,6 @@
-REM This bat file removes the annoying auto-created files from the Windows 10 desktop. It does not delete anything that I think you need, even if that thing you need is still clutter. However, this is still based on my personal judgement that you don't need stuff like .lesshst. I keep this file in util, but put a shortcut to it on my desktop, so I can just click that. It should be run in administrator mode (TODO: a number of these still give "Access is denied." errors for apparently no reason).
-REM The "right" way to do this, which we do, is to delete the files I know about, and then rmdir the folders. That way that only known-bad files and empty dirs are deleted. Just in case. (TODO: I haven't been able to hone in on exactly the right things because I keep getting the Access denied errors.)
+REM This bat file removes the annoying auto-created files from the Windows 10 desktop. It does not delete anything that I think you need, even if that thing you need is still clutter. However, this is still based on my personal judgement that you don't need stuff like .lesshst. I keep this file in util, but put a shortcut to it on my desktop, so I can just click that. It should be run in administrator mode (a number of these still give "Access is denied." errors for apparently no reason).
+REM The "right" way to do this, which we do, is to delete the files I know about, and then rmdir the folders. That way that only known-bad files and empty dirs are deleted. Just in case.
+REM you can fix the problem of windows automatically creating some of these with approaches along the lines described in https://superuser.com/a/1936287 — for instance, to stop it from automatically creating Searches, you would also delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d1d3a04-debb-4115-95cf-2f29da2920da}, and so on.
 rmdir "%USERPROFILE%\Music\"
 rmdir "%USERPROFILE%\Searches\"
 rmdir "%USERPROFILE%\Favorites\"
@@ -12,6 +13,7 @@ del "%USERPROFILE%\.lesshst"
 del "%USERPROFILE%\.python_history"
 del "%USERPROFILE%\.idlerc\recent-files.lst"
 del "%USERPROFILE%\.idlerc\breakpoints.lst"
+del "%USERPROFILE%\.node_repl_history"
 rmdir "%USERPROFILE%\.idlerc\"
 rmdir "%USERPROFILE%\Videos\NVIDIA\"
 rmdir "%USERPROFILE%\Videos\Captures\"

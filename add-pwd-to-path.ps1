159a802b4098a4ea3c724ccac3710320690c602d
wyattscarpenter <wyattscarpenter@gmail.com>
2026-07-12 08:36:09 -0700

    add-pwd-to-path.ps1 probably works version

--- add-pwd-to-path.ps1
+++ add-pwd-to-path.ps1
@@ -1,1 +1,3 @@
-$env:path += ";." # I don't really know if this will persist between sessions, etc. In fact, I'm pretty confident it will not. So maybe I have to mess with some kind of .profile file for Windows? Ideally this would also work for cmd...
+# I haven't yet actually run this program to check if it works, for various reasons. I don't really know if this will persist between sessions, etc. Although I think it will. Ideally this would also work for cmd... so maybe I have to mess with some kind of .profile file for Windows? We'll see.
+$env:path = $env:path + ';' + $pwd
+[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)

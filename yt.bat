c4aedfcf6ce45753abb1a4f7487c168f9de3087b
wyattscarpenter <wyattscarpenter@gmail.com>
2026-08-05 03:19:16 -0700

    forbid ai upscales in yt

--- yt.bat
+++ yt.bat
@@ -1,4 +1,4 @@
 REM A simple alias for yt-dlp (or youtube-dl or any other project carrying the mantle — opaquely to the caller).
 REM Things got complicated enough with cookies and js runtimes that I've just specified those here.
 
-yt-dlp --cookies-from-browser firefox --js-runtime node --remote-components ejs:github %*
+yt-dlp --cookies-from-browser firefox --js-runtime node --remote-components ejs:github -f "bv[format_note!*=AI-upscaled]+ba" %*

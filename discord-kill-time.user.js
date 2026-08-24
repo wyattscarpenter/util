6b2e228eaa8ff9537731fd77428c192cdfd97c9f
wyattscarpenter <wyattscarpenter@gmail.com>
2026-08-24 12:58:55 -0700

    add two retrier scripts that aren't really that useful at all

--- discord-kill-time.user.js
+++ discord-kill-time.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Kill Time
 // @namespace    https://github.com/wyattscarpenter/util
-// @version      8
+// @version      9
 // @author       wyattscarpenter
 // @description  Discord text formatting don't be annoying challenge
 // @match        *://discordapp.com/*
@@ -13,21 +13,32 @@
 //It seems to be quite finicky and I wouldn't have gotten it on my own.
 //His userscript says it's GNU AGPLv3 so maybe that implies this one is also? I don't really know.
 
+//TODO: NOCHECKIN: this is an attempt to remove the crashiness of this script (I don't think it liked it when you changed classes or removed items) but now the script randomly works or doesn't, and I'm not really sure why.
+//Also, I haven't tested this setup in cozy theme
+let colonated_headers = new Set();
+
 function killTime(){ //should work for both compact and cozy.
-  document.querySelectorAll('[class^="latin24CompactTimeStamp"]').forEach(element => element.style.userSelect = "none") //compact mode timestamp
+  document.querySelectorAll('[class^="latin24CompactTimeStamp"]').forEach(element => element.style.userSelect = "none"); //compact mode timestamp
   document.querySelectorAll('[class^="timestamp"]').forEach(element => element.style.userSelect = "none") //cozy mode timestamp
-  document.querySelectorAll('[class^="copyOnlyText"]').forEach(element => element.style.userSelect = "none") //clan tag, at least in compact mode (haven't checked the other one)
-  document.querySelectorAll('[class^="separator"]').forEach(element => element.remove()) //since separator location differs between cozy and compact, it's best just to not use it.
+  document.querySelectorAll('[class^="copyOnlyText"]').forEach(element => element.style.userSelect = "none"); //clan tag, at least in compact mode (haven't checked the other one)
+  //Sometimes there's a leading space before some usernames when you copy them? I think this is due to the alt text on avatars in compact display (which for some reason is " "). Still not sure why it only happens sometimes but at least I can fix it. // This doesn't seem to prevent the problem.
+  document.querySelectorAll('[class^="avatar"]').forEach(element => element.style.userSelect = "none");
+  document.querySelectorAll('[class^="separator"]').forEach(element => element.style.userSelect = "none") //since separator location differs between cozy and compact, it's best just to not use it.
   //Since I still want a copyable colon in cozy mode, we add one:
-  //(We also change the class as a lazy way of preventing an infinite colon-adding loop.)
-  document.querySelectorAll('[class^="headerText"]').forEach(element => {element.firstChild.append(": "); element.className = "colonatedHeaderText";})
+  document.querySelectorAll('[class^="headerText"]').forEach(element => {
+      element.lastChild.remove();
+      if (!colonated_headers.has(element)) {
+          element.firstChild.append(":"); colonated_headers.add(element);
+      }
+  });
 }
 
 var observer = false, observing = false;
 function init() {
   setInterval(function(e) {
     if (!observing && document.querySelector('[class^="separator"]')) { //We delay running the function until this type of element is loaded, so as to avoid bogging down the page-load performance with Observing every change to the DOM before we need to.
-      observing = true;
+        colonated_headers = new Set();
+        observing = true;
       if (!observer) {
           observer = new MutationObserver(killTime);
       }
